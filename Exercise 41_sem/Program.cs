3b31ef637ffe7e3505ce39efb62e8456ed6225ba
Aram <armsir-8182@mail.ru>
2022-11-06 12:58:56 +0300

    Solving the Exercise 41 - finding how much positive numbers entered user

---
+++ Exercise 41_sem/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+int Sum_num = 0;
+
+Console.Write("Enter the number M= ");
+int.TryParse(Console.ReadLine(),out int M);
+
+Console.WriteLine($"Enter {M} numbers: ");
+for(int i = 0; i < M; i++) 
+{
+int.TryParse(Console.ReadLine(),out int Mn);
+if(Mn > 0) Sum_num = Sum_num + 1;
+}
+Console.Write($"    -> {Sum_num}     ---(Вы ввели {Sum_num} чисел больше 0!!!)");
